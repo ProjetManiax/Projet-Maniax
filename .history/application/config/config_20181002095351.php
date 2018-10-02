7c2471d029357dd8227659e6d692db85ef267cc1
unknown <you@example.com>
2018-10-02 10:07:05 +0200

    change database

--- .history/application/config/config_20181002095351.php
+++ .history/application/config/config_20181002095351.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/sio2/Projet-Maniax/';
+$config['base_url'] = 'http://localhost/projetmaniax/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/sio2/Projet-Maniax/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = 'accueil.php';
 
 /*
 |--------------------------------------------------------------------------
